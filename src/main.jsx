ed4ea9efbfc0d65e624777abb9db82c4eec59fcc
Bibi-fathima05 <bibifathima0943@gmail.com>
2026-04-29 11:32:58 +0530

    thirdpart

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { AuthProvider } from './context/AuthContext.jsx';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './lib/queryClient.js';
 import './styles/variables.css';
 import './styles/global.css';
 import './styles/components.css';
@@ -8,6 +11,10 @@ import './styles/animations.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
